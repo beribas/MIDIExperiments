47b3e6348f646a1c25ca7ecdb2f8abbbbd524c9f
Oleg Langer <piligrimo@gmail.com>
2014-04-27 11:13:34 +0200

    Revert "just for a git test"
    This reverts commit 19455097bab4c005039c9dfaefaf4349783f69b2.

--- MIDITests/MIDITests.m
+++ MIDITests/MIDITests.m
@@ -24,7 +24,6 @@
 {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
-    // test commmit
 }
 
 - (void)testExample
